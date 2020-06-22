9edb0d4ff0c1a8ce188d62c376cccec8473e0d9c
zhix <zhix@usc.edu>
2020-06-22 14:19:25 -0700

    Two Sum

--- python/Two Sum.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        dict = {};
-        for i in range(len(nums)):
-            if (target - nums[i]) in dict:
-                return([i,dict[target-nums[i]]])
-            else:
-                dict[nums[i]]=i
