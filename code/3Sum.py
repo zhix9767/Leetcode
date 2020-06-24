c33187a7e66db950cf25fb401befed005347cbe6
zhix <zhix@usc.edu>
2020-06-24 15:09:02 -0700

    3Sum

---
+++ code/3Sum.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        
+        nums.sort()
+        result = []
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            j = i + 1
+            k = len(nums) - 1
+            while j < k:
+                if nums[i] + nums[j] + nums[k] > 0:
+                    k -= 1
+                elif nums[i] + nums[j] + nums[k] < 0:
+                    j += 1
+                else:
+                    result.append([nums[i],nums[j],nums[k]])
+                    j += 1
+                    while j<k and nums[j] == nums[j-1]:
+                        j += 1
+        return result
