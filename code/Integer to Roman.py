36005555b71317874564444537f9248e1ea45a9c
zhix <zhix@usc.edu>
2020-06-24 14:31:59 -0700

    Integer to Roman

---
+++ code/Integer to Roman.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        case1 = ["","I","II","III","IV","V","VI","VII","VIII","IX"]
+        case2 = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+        case3 = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+        case4 = ["", "M", "MM", "MMM"]
+
+        result = case4[num//1000]+case3[(num%1000)//100]+case2[(num%100)//10]+case1[num%10]
+        return result
