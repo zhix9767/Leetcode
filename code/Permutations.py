f7aa954b8d016e4d76f7e25c54c1cc54c59e9d05
zhix <zhix@usc.edu>
2020-06-27 20:32:04 -0700

    Permutations

---
+++ code/Permutations.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        result = []
+        self.permute2(nums, 0, result)
+        return result
+
+    def permute2(self, nums, i, result):
+        if i == len(nums):
+            result.append(list(nums))
+            return
+        for j in range(i, len(nums)):
+            nums[i], nums[j] = nums[j], nums[i]
+            self.permute2(nums, i+1, result)
+            nums[i], nums[j] = nums[j], nums[i]
+
+test = Solution()
+nums = [1,2,3]
+print(test.permute(nums))
