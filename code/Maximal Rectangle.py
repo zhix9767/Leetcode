bc719e91707a92fdb1595411c87a8f2db01dfd44
zhix <zhix@usc.edu>
2020-07-01 23:37:17 -0700

    Maximal Rectangle

--- code/Maximal Rectangle.py
+++ code/Maximal Rectangle.py
@@ -60,3 +60,57 @@ class Solution(object):
             max_area = max(max_area, height * width)
         
         return max_area
+
+    def maximalRectangle2(self, matrix):
+        """
+        :type heights: List[int]
+        :rtype: int
+        """
+        if not matrix or not matrix[0]:
+            return 0
+        n = len(matrix[0])
+        left = [0] * n
+        right = [n] * n
+        height = [0] * n
+        result = 0
+        for i in range(len(matrix)):
+            cur_left = 0
+            cur_right = n
+            for j in range(n-1,-1,-1):
+                if matrix[i][j] == '1':
+                    right[j] = min(right[j],cur_right)
+                else:
+                    right[j] = n
+                    cur_right = j
+            for j in range(n):
+                if matrix[i][j] == '1':
+                    height[j] += 1
+                    left[j] = max(left[j],cur_left)
+                else:
+                    height[j] = 0
+                    left[j] = 0
+                    cur_left = j + 1
+                result = max(result, (right[j]-left[j])*height[j])
+        return result
+
+    def maximalRectangle3(self, matrix):
+        """
+        :type matrix: List[List[str]]
+        :rtype: int
+        """
+        if not matrix or not matrix[0]:
+            return 0
+        n = len(matrix[0])
+        height = [0] * (n + 1)
+        ans = 0
+        for row in matrix:
+            for i in range(n):
+                height[i] = height[i] + 1 if row[i] == '1' else 0
+            stack = [-1]
+            for i in range(n + 1):
+                while height[i] < height[stack[-1]]:
+                    h = height[stack.pop()]
+                    w = i - stack[-1] - 1
+                    ans = max(ans, h * w) 
+                stack.append(i) 
+        return ans
