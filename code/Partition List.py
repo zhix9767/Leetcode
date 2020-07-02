25009504375a2e333d38f44698654b5fcc319dd3
zhix <zhix@usc.edu>
2020-07-01 23:44:02 -0700

    Partition List

---
+++ code/Partition List.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def partition(self, head, x):
+        """
+        :type head: ListNode
+        :type x: int
+        :rtype: ListNode
+        """
+        if not head:
+            return head
+        h1 = l1 = ListNode(0)
+        h2 = l2 = ListNode(0)
+        while head:
+            if head.val < x:
+                l1.next = head
+                l1 = l1.next
+            else:
+                l2.next = head
+                l2 = l2.next
+            head = head.next
+        l2.next = None
+        l1.next=  h2.next
+        return h1.next
